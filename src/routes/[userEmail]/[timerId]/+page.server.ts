cddff1bdd8d3d4c6841457ffcb527ce662164e07
Julian Rivera <dev@juarri.com>
2024-07-19 21:19:09 -0700

    cleanup code

--- src/routes/[userEmail]/[timerId]/+page.server.ts
+++ src/routes/[userEmail]/[timerId]/+page.server.ts
@@ -1,5 +1,7 @@
 import type { PageServerLoad } from './$types';
 
+import { error } from '@sveltejs/kit';
+
 import {
 	getIntervalTimerById,
 	intervalTimerUpdateAccessedAt
@@ -13,27 +15,16 @@ export const load: PageServerLoad = async (event) => {
 	const intervalTimerFormSchema = await superValidate(zod(zIntervalTimerFormSchema));
 
 	const user = event.locals.user;
-	if (!user) {
-		return {
-			status: 401,
-			error: new Error('Unauthorized')
-		};
-	}
 
 	const routeId = event.params.timerId;
 	const intervalTimer = await getIntervalTimerById(routeId);
+
 	if (!intervalTimer) {
-		return {
-			status: 404,
-			error: new Error('Interval timer not found')
-		};
+		return error(404, 'Interval timer not found');
 	}
 
-	if (intervalTimer.userId !== user.id) {
-		return {
-			status: 403,
-			error: new Error('Forbidden')
-		};
+	if (intervalTimer.userId !== user?.id) {
+		return error(403, 'Forbidden');
 	}
 
 	intervalTimerUpdateAccessedAt(routeId, user.id);
--- src/routes/[userEmail]/[timerId]/+page.svelte
+++ src/routes/[userEmail]/[timerId]/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	// import { browser } from '$app/environment';
+	import type { PageData } from './$types';
 
 	import { createIntervalTimerSequence } from '$lib/states/local/intervalTimerSequence.svelte';
 	import { createToggle } from '$lib/states/local/toggle.svelte';
@@ -7,25 +7,19 @@
 	import Desktop from './desktop.svelte';
 	import Mobile from './mobile.svelte';
 
-	import type { IntervalTimer } from '$lib/server/db/schema';
-
-	import type { PageData } from './$types';
-	import type { IntervalTimerFormSchema } from '$lib/components/form/intervalTimer';
-	import type { SuperValidated } from 'sveltekit-superforms';
 	export let data: PageData;
 
-	let intervalTimerFormSchema =
-		data.intervalTimerFormSchema as SuperValidated<IntervalTimerFormSchema>;
-	let intervalTimer = data.intervalTimer as IntervalTimer;
+	let intervalTimerFormSchema = data.intervalTimerFormSchema;
+	let intervalTimer = data.intervalTimer;
 
 	let lock = createToggle(false);
-	let intervalTimerSequence = createIntervalTimerSequence(data.intervalTimer as IntervalTimer);
+	let intervalTimerSequence = createIntervalTimerSequence(intervalTimer);
 </script>
 
 <svelte:head>
 	<title
-		>{intervalTimerSequence.displayTimerTimeRemaining} - {intervalTimerSequence.currentSet.name} - {data
-			.intervalTimer?.title} - Interval Timers</title
+		>{intervalTimerSequence.displayTimerTimeRemaining} - {intervalTimerSequence.currentSet.name} - {intervalTimer?.title}
+		- Interval Timers</title
 	>
 </svelte:head>
 
